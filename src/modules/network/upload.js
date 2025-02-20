cf2f9ababcca020702b64bc4d2c715a37f9c8646
meng <meng.liu1@bthome.com>
2025-02-20 15:32:18 +0800

    修改代码

--- src/modules/network/upload.js
+++ src/modules/network/upload.js
@@ -173,3 +173,90 @@ export async function download(fileUrl, fileName, folder) {
       return {code: -1, data: null, msg: '下载失败'};
     });
 }
+
+// 文件下载 注：此处未做权限判断
+export async function fileDownload(fileUrl, fileName, folder) {
+  if (!fileUrl) {
+    console.warn('---> download 下载地址为空');
+    return {code: -1, data: null, msg: '下载地址为空'};
+  }
+
+  const lastPath = fileUrl.lastIndexOf('/');
+  const lastPoint = fileUrl.lastIndexOf('.');
+  if (!fileName) {
+    if (lastPoint < 0) {
+      console.warn('---> download 文件名为空');
+      return {code: -1, data: null, msg: '文件名为空'};
+    } else {
+      fileName = fileUrl.substring(lastPath + 1);
+    }
+  }
+  if (!fileName.includes('.')) {
+    console.warn('---> download 文件后缀为空');
+    return {code: -1, data: null, msg: '文件后缀为空'};
+  }
+
+  let savePath = ''; // 保存路径
+  let fileType = ''; // 文件后缀
+
+  if (isIos) {
+    savePath = `${RNFS.DocumentDirectoryPath}`;
+  } else {
+    savePath = `${RNFS.DownloadDirectoryPath}/${folder || 'Store'}`;
+  }
+  const exists = await RNFS.exists(savePath);
+  if (!exists) {
+    RNFS.mkdir(savePath);
+  }
+  if (fileUrl && fileUrl.includes('/')) {
+    const lastStr = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
+    if (lastStr.includes('.') && !fileName.includes('.')) {
+      fileType = lastStr.substring(lastStr.lastIndexOf('.'));
+    }
+    // console.log('-------------> fileType', fileType);
+  }
+
+  const downPath = `${savePath}/${fileName}${fileType}`;
+  // 如何文件已存在，则直接返回
+  const isExist = await RNFS.exists(downPath);
+  if (isExist) {
+    console.log('---> 文件已存在');
+    return {code: 200, data: downPath, msg: '文件已存在'};
+  }
+
+  // 下载配置
+  const options = {
+    fromUrl: fileUrl,
+    toFile: downPath,
+    background: true,
+    // headers: { 'cookie': 'info.token' || '' },
+    begin: res => {
+      console.log('---> 下载文件开始 begin');
+      console.log(res.contentLength);
+    },
+    progress: res => {},
+  };
+  // 下载文件
+  const ret = RNFS.downloadFile(options);
+
+  return ret.promise
+    .then(res => {
+      console.log('---> 下载文件成功');
+      if (isIos) {
+        // Toast.showShortCenter('导出文件成功，请在“文件app”中查看')
+      } else {
+        let alert = downPath;
+        if (downPath.indexOf('0') > -1) {
+          alert = downPath.split('0/')[1];
+        }
+        // Toast.showShortCenter('文件下载成功!  路径：' + alert);
+      }
+      console.log(res, downPath);
+      return {code: res.statusCode, data: downPath, msg: '下载成功'};
+    })
+    .catch(err => {
+      console.log('---> catch 下载文件失败');
+      console.log(err);
+      return {code: -1, data: null, msg: '下载失败'};
+    });
+}
