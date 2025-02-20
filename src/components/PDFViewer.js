557d9e128e8f3680d05a30b8250dbee561b3eafd
meng <meng.liu1@bthome.com>
2025-02-20 10:29:41 +0800

    修改代码

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -14,26 +14,43 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import Pdf from 'react-native-pdf';
-// import PDFThumbnailViewer from './PDFThumbnailViewer';
 
-function PDFViewer(props) {
+import {download} from './down';
+
+const pdfUrl =
+  'https://dhstac.com/prod/images/pdf/2023Yearbook.pdf';
+
+function PDFView(props) {
   const [bookmarks, setBookmarks] = useState([]);
-  const source = {
-    uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
-    cache: true,
-  };
+  const [numOfPages, setNumOfPages] = useState('0/0');
+  const [source, setSource] = useState({
+    uri: '/storage/emulated/0/Download/Store/2023Yearbook.pdf',
+    cache: false,
+  });
 
   useEffect(() => {
     // 获取PDF元数据
     const fetchBookmarks = async () => {
-      const pdf = new Pdf();
+      const pdf = new Pdf({source});
       await pdf.loadFile(source.uri);
       const metadata = await pdf.getMetadata();
       setBookmarks(metadata.bookmarks || []);
     };
 
     // fetchBookmarks();
+    // downloadPDF();
   }, []);
+
+  function downloadPDF() {
+    download(pdfUrl, '2023Yearbook.pdf').then(res => {
+      if (res.code == 200 && res.data) {
+        setSource({
+          uri: res.data,
+          cache: true,
+        });
+      }
+    });
+  }
 
   const renderBookmark = ({item}) => (
     // <TouchableOpacity onPress={() => pdf.setPage(item.pageNumber)}>
@@ -42,48 +59,92 @@ function PDFViewer(props) {
     </TouchableOpacity>
   );
 
+  if (!source.uri) {
+    return (
+      <View style={styles.container}>
+        <Text>loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <Pdf
-      source={source}
-      onLoadComplete={(numberOfPages, filePath) => {
-        console.log(`---> pages: ${numberOfPages}`);
-      }}
-      onPageChanged={(page, numberOfPages) => {
-        console.log(`---> Current page: ${page}, total pages: ${numberOfPages}`);
-      }}
-      onError={err => {
-        console.log('--->', err);
-      }}
-      scale={1.5} // 设置缩放比例
-      minScale={1.0} // 设置最小缩放比例
-      maxScale={3.0} // 设置最大缩放比例
-      style={styles.pdf}
-    />
-    /* <FlatList
+    <View style={styles.container}>
+      <Pdf
+        style={styles.pdf}
+        source={source}
+        fitPolicy={0}
+        scale={1.0} // 设置缩放比例
+        minScale={0.5} // 设置最小缩放比例
+        maxScale={3.0} // 设置最大缩放比例
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`---> pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          setNumOfPages(`${page}/${numberOfPages}页`);
+          console.log(`---> Current: ${page}, total pages: ${numberOfPages}`);
+        }}
+        onError={err => {
+          console.log('---> err', err);
+        }}
+      />
+      <View style={styles.floatNum}>
+        <Text style={styles.floatText}>{numOfPages}</Text>
+      </View>
+
+      <TouchableOpacity style={styles.floatMenu}>
+        <Text style={styles.floatText}>目录</Text>
+      </TouchableOpacity>
+      {/* <FlatList
         data={bookmarks}
         renderItem={renderBookmark}
         keyExtractor={(item, index) => index.toString()}
         style={styles.bookmarkList}
-      /> */
+      /> */}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   pdf: {
     flex: 1,
     // width: Dimensions.get('window').width * 0.75,
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height-30,
+    height: Dimensions.get('window').height - 28,
   },
   bookmarkList: {
     flex: 0.25,
-    backgroundColor: '#f0f0f0',
+    backgroundColor: '#676767',
   },
   bookmark: {
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: '#979797',
+  },
+  floatNum: {
+    position: 'absolute',
+    right: 16,
+    top: 24,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 4,
+  },
+  floatText: {
+    color: '#fff',
+    fontSize: 14,
+  },
+  floatMenu: {
+    position: 'absolute',
+    left: 16,
+    top: 24,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 4,
   },
 });
 
-export default PDFViewer;
+export default PDFView;
